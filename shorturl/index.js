742481fb53bb2888a1a0d83a022c4c2fc59aafa6
clydedz <clyd94@gmail.com>
2021-10-07 19:19:21 +1300

    Add a simple path-based URL redirect

--- shorturl/index.js
+++ shorturl/index.js
@@ -1,12 +1,20 @@
-addEventListener('fetch', event => {
+const base = "https://example.com"
+const statusCode = 301
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  const { pathname } = url
+  const destinationURL = base + pathname
+
+  if (pathname === "/") {
+    return new Response("Hello worker!", {
+      headers: { "content-type": "text/plain" }
+    })
+  }
+
+  return Response.redirect(destinationURL, statusCode)
+}
+
+addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-}
